cfcf1b29baccd25d7d5bf3c1d9fad0c5bf6bc5a2
Jikkai Xiao <sonne@asaki.me>
2018-03-15 18:58:48 +0800

    modified toolbar component

--- src/components/Editor/index.vue
+++ src/components/Editor/index.vue
@@ -1,16 +1,21 @@
 <template>
   <section class="hexo-editor">
     <textarea class="hexo-editor__content">{{content.raw}}</textarea>
+    <hexo-editor-toolbar />
   </section>
 </template>
 
 <script>
   import Vue from 'vue'
   import Component from 'vue-class-component'
+  import EditorToolbar from './toolbar'
 
   @Component({
     props: {
       content: Object
+    },
+    components: {
+      [EditorToolbar.name]: EditorToolbar
     }
   })
   export default class HexoEditor extends Vue {
@@ -20,12 +25,15 @@
 <style>
   @component-namespace hexo {
     @c editor {
+      position: relative;
       size: 100%;
+      display: flex;
 
       @d content {
         size: 100%;
-        padding: 12px 24px;
+        padding: 12px 74px 12px 24px;
         font-size: 14px 2;
+        background: inherit;
         border: none;
         box-sizing: border-box;
         display: block;
---
+++ src/components/Editor/toolbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="hexo-editor-toolbar">
+    123
+  </section>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import Component from 'vue-class-component'
+
+  @Component
+  export default class HexoEditorToolbar extends Vue {
+  }
+</script>
+
+<style>
+  @component-namespace hexo-editor {
+    @c toolbar {
+      position: absolute;
+      top: 0;
+      right: 0;
+      size: 50px 100%;
+    }
+  }
+</style>
