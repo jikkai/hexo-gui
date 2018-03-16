228d672ef5166c5882c77f2f460c2cef771d401e
Jikkai Xiao <sonne@asaki.me>
2018-03-16 16:00:16 +0800

    add content meta

--- src/components/Editor/toolbar.vue
+++ src/components/Editor/toolbar.vue
@@ -11,12 +11,19 @@
       </Tooltip>
     </a>
     <a class="hexo-editor-toolbar__item">
-      <Poptip title="文章信息" placement="left">
+      <Poptip title="文章信息" placement="left" width="280">
         <Tooltip content="信息" placement="left">
           <Icon type="information-circled" />
         </Tooltip>
-        <div slot="content">
-          12313123
+        <div class="hexo-editor-toolbar__meta" slot="content">
+          <a target="_blank" :href="meta.path">
+            <Icon type="link" />
+            文章链接
+          </a>
+          <p>字数：{{meta.count}}</p>
+          <p>段落：{{meta.phase}}</p>
+          <p>更新时间：{{$date.format(meta.updated)}}</p>
+          <p>创建时间：{{$date.format(meta.date)}}</p>
         </div>
       </Poptip>
     </a>
@@ -33,6 +40,20 @@
     name: 'hexo-editor-toolbar',
     props: {
       content: Object
+    },
+    computed: {
+      meta () {
+        const { updated, date, content, _content, path } = this.content
+        if (!path) return {}
+
+        // 统计段落数
+        const phase = content.split('<p>').length - 1
+        // 统计字数
+        const count = Array.from(_content.split(''), str => escape(str).length)
+          .reduce((a, b) =>b.length > 4 ? a + 2 : a + 1)
+
+        return { updated, date, phase, count, path }
+      }
     }
   }
 </script>
@@ -40,8 +61,8 @@
 <style>
   @component-namespace hexo-editor {
     @c toolbar {
-      position: absolute;
-      top: 0;
+      position: fixed;
+      top: 64px;
       right: 0;
       size: 50px 100%;
       font-size: 20px;
@@ -49,11 +70,14 @@
       @d item {
         height: 50px;
         color: inherit;
-        /* line-height: 50px;
-        text-align: center; */
         display: flex;
         justify-content: center;
         align-items: center;
+      }
+
+      @d meta {
+        color: #495060!important;
+        cursor: default;
       }
     }
   }
